2d515509dd567f0872933f1ee6dc39487668ab06
Alejandro <alejandro6chaves@gmail.com>
2024-06-10 02:29:07 -0600

    ale crea seccion de tablas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatDialogModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [CommonModule,RouterModule, MatButtonModule, MatDialogModule, ReactiveFormsModule],
+  templateUrl:'./home.component.html',
+  styleUrl:'./home.component.css'
 })
 export class HomeComponent {
- 
-
+  cuerpo: string = '';
   
+
+  eleccion(choice:string){
+    this.cuerpo = choice
+  } 
 }
 
