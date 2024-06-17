4da04f8d18f76231be27a832e004b9d0c1122a65
Alejandro <alejandro6chaves@gmail.com>
2024-06-16 19:15:18 -0600

    responsive de asientos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,15 @@ import { IndexComponent } from './components/home/index/index.component';
 import { PeliculaComponent } from './components/pelicula/pelicula.component';
 import { UsuarioAdministracionComponent } from './components/home/usuario-administracion/usuario-administracion.component';
 import { ComidaAdministracionComponent } from './components/home/comida-administracion/comida-administracion.component';
+import { AsientosComponent } from './components/home/asientos/asientos.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent, children:[
         {path:'',component:IndexComponent},
         {path:'combos',component:CombosComponent},
-<<<<<<< HEAD
         {path:'peliculas/:id', component:PeliculaComponent},
-        {path:'usuarioAdministracion', component:UsuarioAdministracionComponent}]
-=======
-        {path:'peliculas', component:PeliculaComponent},
         {path:'usuarioAdministracion', component:UsuarioAdministracionComponent},
-        {path:'comidaAdministracion',component:ComidaAdministracionComponent}]
->>>>>>> 42b3a5e2d7b167d2734edea6e4672890a8acbc07
-       
+        {path:'comidaAdministracion',component:ComidaAdministracionComponent},
+        {path:'asientos/:id',component:AsientosComponent}],
     }
 ];
